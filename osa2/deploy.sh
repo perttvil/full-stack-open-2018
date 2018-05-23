aea70a412cb960f65bf573eec5e0401e85b85f01
perttvil <vili.perttila@affecto.com>
2018-05-23 12:13:01 +0300

    Fixed path to osa3 git repo

--- osa2/deploy.sh
+++ osa2/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 npm run build
 
-rm -rf ../osa3/build
-rm -rf ../../heroku-api/build
-
 cp -r build ../osa3/ -v
-cp -r build ../../heroku-api/ -v
+cp -r build ../../full-stack-open-2018-osa3/ -v
